e5f1c01cfb961097404ce6e5d5d4811ea1c80a4a
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-08-18 13:44:42 +0800

    build(vite): add fonts configurations

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Fonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    Fonts({
+      google: {
+        name: 'lato',
+        styles: 'wght@100;300;400;700;900'
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
+  },
+  server: {
+    port: 3000
   }
 })
