de7ad6491b20f8f19a155258157bbaee7b346bf8
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-08-22 21:01:00 +0800

    feat(components): add compose message

---
+++ src/components/ComposeMessage/ComposeMessage.vue
@@ -0,0 +1,23 @@
+<script setup>
+import VBtn from '../Buttons/VBtn.vue'
+
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: 'Type a new message'
+  }
+})
+
+const emits = defineEmits(['onSend'])
+</script>
+
+<template>
+  <div class="compose-message__wrap">
+    <input class="form__control" type="text" :placeholder="placeholder" />
+    <v-btn @click="$emit('onSend')" caption="Send" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import './styles.scss';
+</style>
