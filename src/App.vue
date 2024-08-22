2ed089ec72ea9b1cea1bcb2f9ca78b007f9ad38b
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-08-22 10:15:23 +0800

    feat(components): add SearchBar

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,8 @@
-<template>
-  <main>
-    <div class="quicks">
-      <VFab
-        icon="oui:bolt"
-        class="main__fab"
-        :class="showQuicks && 'reveal-item'"
-        @click="handleRevealQuicks"
-      />
-      <VFab
-        icon="material-symbols:forum-outline"
-        color="indicator-2"
-        label="Inbox"
-        fab-type="item"
-        :hide-label="activeQuicks"
-        :class="activeQuicks === 'inbox' ? 'is-active' : ''"
-        :is-active="activeQuicks === 'inbox'"
-        @click="handleActiveQuicks('inbox')"
-        @close="handleCloseQuicks()"
-      />
-      <VFab
-        icon="material-symbols:chrome-reader-mode-outline"
-        color="indicator-1"
-        label="Task"
-        fab-type="item"
-        :hide-label="activeQuicks"
-        :class="activeQuicks === 'task' ? 'is-active' : ''"
-        :is-active="activeQuicks === 'task'"
-        @click="handleActiveQuicks('task')"
-        @close="handleCloseQuicks()"
-      />
-    </div>
-  </main>
-</template>
-
 <script setup>
 import { ref } from 'vue'
 
 import VFab from '@/components/Buttons/VFab.vue'
+import SearchBar from '@/components/SearchBar/SearchBar.vue'
 
 import './assets/styles.scss'
 
@@ -116,3 +82,43 @@ function handleRevealQuicks() {
   resolveRevealQuicks(quicks)
 }
 </script>
+
+<template>
+  <main>
+    <div class="quicks__dialog">
+      <div class="dialog__body">
+        <SearchBar placeholder="Search" />
+      </div>
+    </div>
+    <div class="quicks">
+      <VFab
+        icon="oui:bolt"
+        class="main__fab"
+        :class="showQuicks && 'reveal-item'"
+        @click="handleRevealQuicks"
+      />
+      <VFab
+        icon="material-symbols:forum-outline"
+        color="indicator-2"
+        label="Inbox"
+        fab-type="item"
+        :hide-label="activeQuicks"
+        :class="activeQuicks === 'inbox' ? 'is-active' : ''"
+        :is-active="activeQuicks === 'inbox'"
+        @click="handleActiveQuicks('inbox')"
+        @close="handleCloseQuicks()"
+      />
+      <VFab
+        icon="material-symbols:chrome-reader-mode-outline"
+        color="indicator-1"
+        label="Task"
+        fab-type="item"
+        :hide-label="activeQuicks"
+        :class="activeQuicks === 'task' ? 'is-active' : ''"
+        :is-active="activeQuicks === 'task'"
+        @click="handleActiveQuicks('task')"
+        @close="handleCloseQuicks()"
+      />
+    </div>
+  </main>
+</template>
---
+++ src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+
+import '@/components/SearchBar/styles.scss'
+
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div class="form__control">
+    <input type="text" :placeholder="placeholder" />
+    <span class="form__icon">
+      <Icon icon="mdi:magnify" />
+    </span>
+  </div>
+</template>
