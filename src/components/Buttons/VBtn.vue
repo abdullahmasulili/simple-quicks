7197781281a883aaccb652d1f4e161c4abc53728
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-08-22 20:47:45 +0800

    feat(Buttons): add regular button

---
+++ src/components/Buttons/VBtn.vue
@@ -0,0 +1,18 @@
+<script setup>
+import '@/components/Buttons/styles.scss'
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: 'primary-1'
+  },
+  caption: {
+    type: String,
+    default: 'Click Me'
+  }
+})
+</script>
+
+<template>
+  <button class="btn" :class="`color--${color}`">{{ caption }}</button>
+</template>
