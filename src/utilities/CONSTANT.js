2d7f09a199c96f685e95c917c2d8dfca31e14e70
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-08-23 14:01:34 +0800

    feat(utilities): add constant values

---
+++ src/utilities/CONSTANT.js
@@ -0,0 +1,19 @@
+const COLORS = {
+  PRIMARY_1: '#2f80ed',
+  PRIMARY_2: '#4f4f4f',
+  PRIMARY_3: '#828282',
+  PRIMARY_4: '#e0e0e0',
+  INDICATOR_1: '#f8b76b',
+  INDICATOR_2: '#8785ff',
+  INDICATOR_3: '#eb5757',
+  INDICATOR_4: '#f2c94c',
+  STICKER_1: '#e9f3ff',
+  STICKER_2: '#fdcfa4',
+  STICKER_3: '#f9e9c3',
+  STICKER_4: '#afebdb',
+  STICKER_5: '#cbf1c2',
+  STICKER_6: '#cfcef9',
+  STICKER_7: '#f9e0fd'
+}
+
+export { COLORS }
